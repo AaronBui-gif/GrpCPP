1209c22a4bbb315367d789390a21fb246f94103a
Aaron Bui <s3740934@rmit.edu.vn>
2021-03-16 20:45:41 +0700

    done

---
+++ cmake-build-debug/CMakeFiles/GrpCPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GrpCPP.dir/main.cpp.o"
+  "GrpCPP"
+  "GrpCPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GrpCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
